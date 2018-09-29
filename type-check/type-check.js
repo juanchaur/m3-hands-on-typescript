94b8cbfdfc81232f6d0581c81b2444df8c1301d0
juanchaur1 <juan.chaur@marfeel.com>
2018-09-29 14:59:50 -0400

    adding first demo

---
+++ type-check/type-check.js
@@ -0,0 +1,13 @@
+function parse(input) {
+	output = '';
+
+	for (var i = 0; i < input.length; i++) {
+		output += input[i];
+	}
+
+	output = i;
+	return output;
+}
+
+parse('hello m3');
+parse(['hello', 'm3']);
